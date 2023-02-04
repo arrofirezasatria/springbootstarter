72b4866fe6afffdcfb230367d8716e3d58228227
rofireza <arrofirezasatria@gmail.com>
2023-02-04 07:38:49 +0700

    update

---
+++ src/main/java/com/rofireza/springbootstarter/AppGamingBasicJava.java
@@ -0,0 +1,9 @@
+package com.rofireza.springbootstarter;
+
+public class AppGamingBasicJava {
+    public staic void main(string[] args){
+        var marioGame = new MarioGame();
+        var gameRunner = new GameRunner();
+        gameRunner.run()
+    }
+}
